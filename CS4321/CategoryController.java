1daf6d3c0d11b45a6fa5034c10b352a4c0af598d
Khristian Jaime <142678663+ksjaime@users.noreply.github.com>
2024-10-17 10:34:57 -0400

    Created a CategoryController Class to manage adding categories.

---
+++ CS4321/CategoryController.java
@@ -0,0 +1,20 @@
+package CS4321;
+
+import java.util.List;
+
+public class CategoryController {
+    private List<Category> categories;
+
+    public CategoryController(List<Category> categories){
+        this.categories = categories;
+    }
+
+    public void addCategory (String categoryName){
+        Category newCategory = new Category(categoryName);
+        categories.add(newCategory);
+    }
+
+    public List<Category> getCategories(){
+        return categories;
+    }
+}
