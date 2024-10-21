18e1c1f50010ea393295ab1d4bc245146bb9b039
Khristian Jaime <142678663+ksjaime@users.noreply.github.com>
2024-10-21 12:10:53 -0400

    Commission Class -  handles the seller’s commission percentage

---
+++ CS4321/Commission.java
@@ -0,0 +1,17 @@
+package CS4321;
+
+public class Commission {
+    private double sellerCommission;
+
+    public Commission (double sellerCommission){
+        this.sellerCommission = sellerCommission;
+    }
+
+    public double getSellerCommission(){
+        return sellerCommission;
+    }
+
+    public void setSellerCommission(double sellerCommission){
+        this.sellerCommission = sellerCommission;
+    }
+}
