c5f36f6235ad331bbf7cc8e8d669ab1bcd6376ab
Khristian Jaime <142678663+ksjaime@users.noreply.github.com>
2024-10-28 11:30:16 -0400

    Added 2 variables, updated the Constructor, added 2 getter methods.

--- CS4321/Item.java
+++ CS4321/Item.java
@@ -1,29 +1,44 @@
 package CS4321;
 
+import java.time.LocalDate;
+
 public class Item {
     private String name;
     private double startingPrice;
+    private LocalDate endDate;
+    private double shippingCost;
     private boolean isActive;
 
-    public Item(String name, double startingPrice){
+    public Item(String name, double startingPrice, LocalDate endDate, double shippingCost) {
         this.name = name;
         this.startingPrice = startingPrice;
+        this.endDate = endDate;
+        this.shippingCost = shippingCost;
         this.isActive = true;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getStartingPrice(){
+    public double getStartingPrice() {
         return startingPrice;
     }
 
-    public boolean isActive(){
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public double getShippingCost() {
+        return shippingCost;
+    }
+
+    public boolean isActive() {
         return isActive;
     }
 
-    public void endAuction(){
+    public void endAuction() {
         this.isActive = false;
     }
 }
+
